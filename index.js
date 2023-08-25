8d4f4d60b59b0165c19440998c04b34fe84702c5
Gustavo <gutomelo53@gmail.com>
2023-08-25 02:53:58 +0200

    Alteração index.js feita

---
+++ index.js
@@ -0,0 +1,16 @@
+/*
+//Exercicio 1
+
+function minhaFuncao(variavel) {
+	return variavel * 5//multiplica o numero recebido
+}
+
+console.log(minhaFuncao(2))//envia o numero 2 para a função e imprimi na tela o resultado: 10
+console.log(minhaFuncao(10))//envia o numero 10 para a função e imprimi na tela o resultado 20
+
+vai imprimir os valores 10 e 20
+
+se tirasse o console.log irá apenas chamar e enviar os valores a função
+*/
+
+//Exercicio 2
