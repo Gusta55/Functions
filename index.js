1e67c43924b09fcf416bd56969a7a5d540a497e8
Gustavo <gutomelo53@gmail.com>
2023-08-25 23:46:01 +0200

    Alteração index.js feita

--- index.js
+++ index.js
@@ -14,3 +14,38 @@ se tirasse o console.log irá apenas chamar e enviar os valores a função
 */
 
 //Exercicio 2
+/*
+let textoDoUsuario = prompt("Insira um texto"); //Pede para o ususario retornar um valor
+
+const outraFuncao = function(texto) { // função declarada
+	return texto.toLowerCase().includes("cenoura")// converte o texto para caixa baixa e verifica se há a palavra "cenoura"
+}
+
+const resposta = outraFuncao(textoDoUsuario) // chama a função
+console.log(resposta)//printa a resposta
+
+i.   Eu gosto de cenoura//true
+ii.  CENOURA é bom pra vista//true
+iii. Cenouras crescem na terra//true
+*/
+
+//Exercicios escritos
+//exercicio 1
+/*
+function usuario() {
+	console.log("Eu sou Gustavo, tenho 22 anos, moro em São Leopoldo e sou estudante.");
+}
+
+usuario();
+*/
+
+function usuario(nome , idade, cidade, profissao){
+	console.log(`Eu sou ${nome}, tenho ${idade} anos, moro ${cidade} e sou ${profissao}.`)
+
+}
+
+const nome1 = prompt("Digite o seu nome: ");
+const idade1 = prompt("Digite a sua idade: ");
+const cidade1 = prompt("Digite a sua cidade: ");
+const profissao1 = prompt("DIgite a sua profissao: ");
+usuario(nome1, idade1, cidade1, profissao1);
